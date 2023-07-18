7b06f15cc0288ce916f1466e2853cbc5f15c0568
PashaLopatuk <60324515+PashaLopatuk@users.noreply.github.com>
2023-07-18 16:07:33 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       {/* <BackgroundCircles /> */}
       <Header/>
       <Main />
-      <Footer/>
+      {/* <Footer/> */}
 
     </main>
   )
--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { twMerge } from 'tailwind-merge';
+
 import React from 'react'
 
 interface BackgroundCirclesProps {
@@ -6,9 +9,30 @@ interface BackgroundCirclesProps {
 }
 
 const BackgroundCircles: React.FC<BackgroundCirclesProps> = ({ className }) => {
+  
+  const words = [
+    'developer',
+    'div',
+    'className',
+    'Typescript',
+    'front-end',
+    'dev',
+    'html',
+    'tailwind',
+    'css'
+  ]
+  
   return (
-    <div className={`${className} bg-black z-0 fixed w-full h-full left-0 top-0 `}>
-        
+    <div className={`${className} bg-gray-900 z-[100] fixed w-full h-full left-0 top-0 `}>
+      
+    {
+      words.map( (e) => 
+        <div className={twMerge(
+          'text-[', String(Math.floor(Math.random() * 10) * 30), 'px] absolute left-[', String(Math.floor(Math.random() * 100)), '%]top-[', String(Math.floor(Math.random() * 100)), '%]')} >
+          { e }
+        </div>
+      )
+    }
         
     </div>
   )
